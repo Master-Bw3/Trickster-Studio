7cdb68ef7b362f875141382bc71739a666905bb8
Master-Bw3 <bw1jochman@gmail.com>
2025-01-15 22:43:01 -0600

    rm

--- WasmEndec/src/main/java/maple/trickster_endec/spell_instruction/EnterScopeInstruction.java
+++
@@ -1,8 +0,0 @@
-package maple.trickster_endec.spell_instruction;
-
-public class EnterScopeInstruction implements SpellInstruction {
-    @Override
-    public SerializedSpellInstruction asSerialized() {
-        return new SerializedSpellInstruction(SpellInstructionType.ENTER_SCOPE, null);
-    }
-}
--- WasmEndec/src/main/java/maple/trickster_endec/spell_instruction/ExitScopeInstruction.java
+++
@@ -1,8 +0,0 @@
-package maple.trickster_endec.spell_instruction;
-
-public class ExitScopeInstruction implements SpellInstruction {
-    @Override
-    public SerializedSpellInstruction asSerialized() {
-        return new SerializedSpellInstruction(SpellInstructionType.EXIT_SCOPE, null);
-    }
-}
