e8430a8fef2f1c5e8e1ba0bed54edff3995258b1
Master-Bw3 <bw1jochman@gmail.com>
2025-01-28 22:51:06 -0600

    performance

--- src/components/SpellDisplay.tsx
+++ src/components/SpellDisplay.tsx
@@ -1,17 +1,28 @@
-import { Application, Assets, Texture, Ticker } from "pixi.js";
-import SpellPartWidget from "./spellDisplay/SpellPartWidget";
-import RevisionContext from "../RevisionContext";
-import SpellPart from "../fragment/SpellPart";
-import { Accessor, createEffect, createUniqueId, JSX, onMount, Setter, splitProps } from "solid-js";
+import { Application, Assets, Texture, Ticker } from 'pixi.js';
+import SpellPartWidget from './spellDisplay/SpellPartWidget';
+import RevisionContext from '../RevisionContext';
+import SpellPart from '../fragment/SpellPart';
+import { Accessor, createEffect, createUniqueId, JSX, onMount, Setter, splitProps } from 'solid-js';
 
-type Props = { spellPart: Accessor<SpellPart>; setSpellPart: Setter<SpellPart>; fixedPosition?: boolean; isMutable?: boolean; initialScale?: number } & JSX.HTMLAttributes<HTMLDivElement>
+type Props = {
+    spellPart: Accessor<SpellPart>;
+    setSpellPart: Setter<SpellPart>;
+    fixedPosition?: boolean;
+    isMutable?: boolean;
+    initialScale?: number;
+} & JSX.HTMLAttributes<HTMLDivElement>;
 
 function SpellDisplay(props: Props) {
-    const [local, other] = splitProps(props, ["spellPart", "setSpellPart", "fixedPosition", "isMutable", "initialScale"])
+    const [local, other] = splitProps(props, [
+        'spellPart',
+        'setSpellPart',
+        'fixedPosition',
+        'isMutable',
+        'initialScale',
+    ]);
 
     let container: HTMLDivElement;
     let canvas: HTMLCanvasElement;
-
 
     onMount(async () => {
         const app = new Application();
@@ -26,45 +37,51 @@ function SpellDisplay(props: Props) {
         });
 
         await Assets.load({
-            src: "slkscr.ttf",
+            src: 'slkscr.ttf',
             data: {
-                family: "slkscr",
+                family: 'slkscr',
             },
         });
 
-
         const textures: Map<string, Texture> = new Map();
 
-        textures.set("circle_48", await Assets.load("./circle_48.png"));
-        textures.get("circle_48")!.source.scaleMode = "nearest";
-        textures.set("overlay", await Assets.load("./pattern_literal.png"));
-        textures.get("overlay")!.source.scaleMode = "nearest";
+        textures.set('circle_48', await Assets.load('./circle_48.png'));
+        textures.get('circle_48')!.source.scaleMode = 'nearest';
+        textures.set('overlay', await Assets.load('./pattern_literal.png'));
+        textures.get('overlay')!.source.scaleMode = 'nearest';
 
-        const widget = new SpellPartWidget(local.spellPart, local.setSpellPart, app.canvas.width / 2, app.canvas.height / 2, app.canvas.height * (local.initialScale ?? 1), new RevisionContext(), true, (local.fixedPosition ?? false), (local.isMutable ?? true));
+        const widget = new SpellPartWidget(
+            local.spellPart,
+            local.setSpellPart,
+            app.canvas.width / 2,
+            app.canvas.height / 2,
+            app.canvas.height * (local.initialScale ?? 1),
+            new RevisionContext(),
+            true,
+            local.fixedPosition ?? false,
+            local.isMutable ?? true
+        );
 
+        createEffect(() => widget.setSpell(local.spellPart()));
 
         app.ticker.add(() => {
             if (widget.fixedPosition) {
-                widget.size = widget.toScaledSpace(app.canvas.height * (local.initialScale ?? 1))
+                widget.size = widget.toScaledSpace(app.canvas.height * (local.initialScale ?? 1));
                 widget.x = widget.toScaledSpace(app.canvas.width / 2);
                 widget.y = widget.toScaledSpace(app.canvas.height / 2);
             }
 
-            widget.setSpell(local.spellPart())
             widget.render(app.stage, 0, app.canvas.height, textures);
 
-            app.queueResize()
-
-        })
-
-
+            app.queueResize();
+        });
 
         let pinchZooming = false;
         let lastPinchDistance = 0;
 
-        canvas!.addEventListener("wheel", (e) => {
+        canvas!.addEventListener('wheel', (e) => {
             const rect = container!.getBoundingClientRect();
-            const [x, y] = [e.x - rect.left, e.y - rect.top]
+            const [x, y] = [e.x - rect.left, e.y - rect.top];
 
             widget.mouseScrolled(x, y, -e.deltaY / 100);
 
@@ -74,29 +91,32 @@ function SpellDisplay(props: Props) {
         const activeTouches = new Map();
         let mouseDown = () => activeTouches.size != 0;
 
-        canvas!.addEventListener("pointerdown", (e) => {
+        canvas!.addEventListener('pointerdown', (e) => {
             const rect = container!.getBoundingClientRect();
-            const [x, y] = [e.x - rect.left, e.y - rect.top]
+            const [x, y] = [e.x - rect.left, e.y - rect.top];
 
             activeTouches.set(e.pointerId, { x: x, y: y });
 
-            if (e.pointerType === "touch" && activeTouches.size === 2) {
+            if (e.pointerType === 'touch' && activeTouches.size === 2) {
                 pinchZooming = true;
                 const touchPoints = Array.from(activeTouches.values());
-                lastPinchDistance = Math.hypot(touchPoints[1].x - touchPoints[0].x, touchPoints[1].y - touchPoints[0].y);
+                lastPinchDistance = Math.hypot(
+                    touchPoints[1].x - touchPoints[0].x,
+                    touchPoints[1].y - touchPoints[0].y
+                );
             } else {
                 pinchZooming = false;
             }
 
-            widget.dragDrawing = e.pointerType === "touch";
+            widget.dragDrawing = e.pointerType === 'touch';
 
             const button = e.button;
             widget.mouseClicked(x, y, button);
         });
 
-        window.addEventListener("pointerup", (e) => {
+        window.addEventListener('pointerup', (e) => {
             const rect = container!.getBoundingClientRect();
-            const [x, y] = [e.x - rect.left, e.y - rect.top]
+            const [x, y] = [e.x - rect.left, e.y - rect.top];
 
             if (mouseDown()) {
                 const button = e.button;
@@ -109,17 +129,19 @@ function SpellDisplay(props: Props) {
             }
         });
 
-        window.addEventListener("pointermove", (e) => {
+        window.addEventListener('pointermove', (e) => {
             const rect = container!.getBoundingClientRect();
-            const [x, y] = [e.x - rect.left, e.y - rect.top]
+            const [x, y] = [e.x - rect.left, e.y - rect.top];
 
             if (pinchZooming && activeTouches.size === 2) {
-                widget.stopDrawing()
+                widget.stopDrawing();
 
-                const previousTouchPoints = Array.from(activeTouches.entries()).map(([id, touch]) => ({
-                    x: touch.x,
-                    y: touch.y,
-                }));
+                const previousTouchPoints = Array.from(activeTouches.entries()).map(
+                    ([id, touch]) => ({
+                        x: touch.x,
+                        y: touch.y,
+                    })
+                );
 
                 if (activeTouches.has(e.pointerId)) {
                     activeTouches.set(e.pointerId, { x: x, y: y });
@@ -127,11 +149,18 @@ function SpellDisplay(props: Props) {
 
                 const touchPoints = Array.from(activeTouches.values());
 
-                const currentPinchDistance = Math.hypot(touchPoints[1].x - touchPoints[0].x, touchPoints[1].y - touchPoints[0].y);
+                const currentPinchDistance = Math.hypot(
+                    touchPoints[1].x - touchPoints[0].x,
+                    touchPoints[1].y - touchPoints[0].y
+                );
                 const zoomFactor = currentPinchDistance / lastPinchDistance;
                 const scrollDelta = (zoomFactor - 1) * 20;
 
-                widget.mouseScrolled((touchPoints[0].x + touchPoints[1].x) / 2, (touchPoints[0].y + touchPoints[1].y) / 2, scrollDelta);
+                widget.mouseScrolled(
+                    (touchPoints[0].x + touchPoints[1].x) / 2,
+                    (touchPoints[0].y + touchPoints[1].y) / 2,
+                    scrollDelta
+                );
 
                 lastPinchDistance = currentPinchDistance;
 
@@ -161,13 +190,13 @@ function SpellDisplay(props: Props) {
 
             widget.mouseMoved(x, y);
         });
-    })
+    });
 
-
-
-    return <div ref={container!} {...other}>
-        <canvas ref={canvas!} />
-    </div>;
+    return (
+        <div ref={container!} {...other}>
+            <canvas ref={canvas!} />
+        </div>
+    );
 }
 
 export { SpellDisplay };
